9e461cda522d474c40c359d4e53912a9351bb8bb
Egori11a <114509561+Egori11a@users.noreply.github.com>
2023-06-30 18:04:29 +0300

    Add files via upload

--- Potapov/lab24/include/Node.hpp
+++ Potapov/lab24/include/Node.hpp
@@ -1,15 +1,19 @@
 #ifndef NODE_HPP_INCLUDED
 #define NODE_HPP_INCLUDED
-#include <bits/stdc++.h>
-using namespace std;
-
-struct Node {
-    Node* left;
-    Node* right;
-    string value;
-    Node(string val);
+class Node
+{
+public:
+    char token;
+    Node *left = nullptr;
+    Node *right = nullptr;
+    Node() = default;
+    ~Node() = default;
+    Node(const char data);
+    Node(const char data, Node *_left, Node *_right);
+    char get_token() const;
+    Node* get_left() const;
+    Node* get_right() const;
+    void rewrite_node(Node* temp_node);
 };
-
 #include "../src/Node.cpp"
-
 #endif// NODE_HPP_INCLUDED
